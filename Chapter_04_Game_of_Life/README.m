7d2d4ed11c9e4dd9752bb01c46ddc5e4a1d18192
Aykut Argun <48754174+argunaykut@users.noreply.github.com>
2021-10-19 13:29:24 +0200

    Update README.m

--- Chapter_04_Game_of_Life/README.m
+++ Chapter_04_Game_of_Life/README.m
@@ -1,1 +1,3 @@
-# Information about codes
+## Game of Life
+
+This is a Jupyter notebook script that creates a live animation of game of life. You can use buttons to reset to different initial conditions. 
